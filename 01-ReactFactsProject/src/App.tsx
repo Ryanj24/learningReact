f9b59f50028f660236eb3ba8b058e7a3026fbcfe
Ryanj24 <57965902+Ryanj24@users.noreply.github.com>
2024-11-22 16:40:45 +0000

    Added initial Fact page layout and content

--- 01-ReactFactsProject/src/App.tsx
+++ 01-ReactFactsProject/src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
+import ReactLogo from '../src/assets/React-icon.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-red-400'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='bg-slate-600 h-screen'>
+      {/* Navbar */}
+      <nav className='bg-slate-800 p-10'>
+        <div className='flex items-center space-x-2'>
+          <img src={ReactLogo} alt='React Logo SVG' className='h-10 w-10'/>
+          <h1 className='text-[#61DBFB] text-3xl'>React Facts</h1>
+        </div>
+      </nav>
+
+      {/* Facts List */}
+      <section className='flex justify-between items-center w-[900px] max-w-[900px] mx-auto my-20 relative'>
+        <div className='flex flex-col'>
+          <h1 className='text-white text-5xl'>Fun Facts About React</h1>
+          <ul className='p-10 space-y-5 list-disc text-xl'>
+            <li className='text-[#61DBFB]'><span className='text-white'>Was first released in 2013</span></li>
+            <li className='text-[#61DBFB]'><span className='text-white'>Was originally created by Jordan Walke</span></li>
+            <li className='text-[#61DBFB]'><span className='text-white'>Used by many developers worldwide</span></li>
+            <li className='text-[#61DBFB]'><span className='text-white'>Has a vast ecosystem of libraries</span></li>
+            <li className='text-[#61DBFB]'><span className='text-white'>Can be used to create mobile apps</span></li>
+          </ul>
+        </div>
+          <img src='/src/assets/React-icon.svg' className='h-80 w-80 '/>
+      </section>
+    </main>
   )
 }
 
